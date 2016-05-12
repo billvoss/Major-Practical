f87fec965d053f738f861e41b7d1f45fd73e8563
billvoss <billydvoss@gmail.com>
2016-05-12 15:38:12 +0930

    Dealer

--- dealer.cpp
+++ dealer.cpp
@@ -1,5 +1,21 @@
+#include dealer.h
+#include person.h
+#include player.h
+
+#include <iostream>
+
+using namespace std;
+
 dealer::dealer()
 {
+  //creates a vector filled with two decks of cards in sequential order
+  for(int i=0; i<2; i++)
+  {
+    for(int card = 1;card<53;card++)
+    {
+      cards.pushback(card);
+    }
+  }
 }
 
 leaveTable()
@@ -9,5 +25,6 @@ leaveTable()
 
 printStatus()
 {
-  
+  cout<<"I am a dealer."<<endl
+  cout<<"Name:"<<name<<endl;
 }
