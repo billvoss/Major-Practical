99f204e31cbde10a2aedd706febf517c6f1c1d55
calvin123456 <a1708076@student.adelaide.edu.au>
2016-05-12 15:47:52 +0930

    Update table.h

--- table.h
+++ table.h
@@ -1,14 +1,18 @@
 #ifndef table_h
 #define table_h
 
+#include "dealer.h"
+
 class table
 {
 public:
-	table(dealer dealer1); //class constructor receieves a dealer object
+	table(dealer game_dealer); //class constructor receieves a dealer object
 	int getID() //Return the table ID number
 	~table(); //class destructor
 protected:
 private:
-	static int ID; 
+	static int ID;
+	int table_id;
+	dealer my_dealer
 };
 #endif
